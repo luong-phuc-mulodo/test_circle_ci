bc1198c5ecc265e45fc45ac6d7d2314b5fda1b46
luong-phuc-mulodo <luong.phuc@mulodo.com>
2018-11-14 15:37:12 +0700

    Add time for url

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
 import grequests
+import time
 from bs4 import BeautifulSoup
 
-def run_test(time):
-	urls = [
+def run_test(times):
+	links = [
 		'https://search.rakuten.co.jp/search/mall/%E3%82%BF%E3%83%B3%E3%83%96%E3%83%A9%E3%83%BC%20%E5%90%8D%E5%85%A5%E3%82%8C',
 		'https://search.rakuten.co.jp/search/mall/iphone8plus%20%E5%90%8D%E5%85%A5%E3%82%8C',
 		'https://search.rakuten.co.jp/search/mall/galaxy%20s8%20%E3%82%B1%E3%83%BC%E3%82%B9',
@@ -20,7 +21,12 @@ def run_test(time):
 		# 'https://search.rakuten.co.jp/search/mall/iphones'
 	]
 
-	print('Get data from Rakuten time %s' % time)
+	urls = []
+	for link in links:
+		urls.append("%s/?t=%s" % (link,int(time.time())))
+
+	print(urls)
+	print('Get data from Rakuten time %s' % times)
 
 	headers = {'user-agent': 'user-agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'}
 
@@ -45,6 +51,6 @@ def get_data(content):
 		print(link.get('href'))
 
 
-total = 1
-for time in range(0,total):
-	run_test(time+1)
+total = 40
+for times in range(0,total):
+	run_test(times+1)
